95e8eef719515cb80efe7d07dc955e2d0c3a514b
dehypnosis <dehypnosis@gmail.com>
2017-11-09 17:19:35 +0900

    withMediaContext sugar function

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 //
 import ContextProvider from './ContextProvider';
 import MediaQuery from './MediaQuery';
+import MediaQwithMediaContextuery from './withMediaContext';
 
 export {
   ContextProvider,
   MediaQuery,
+  withMediaContext,
 };
 
 export default MediaQuery;
